5a762c6ebc00367997a4522b8916cb6ab805a693
Nicolas <nicolasdeguido@gmail.com>
2024-10-21 17:07:02 +1100

    created endpoint for stations

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 
-const express = require('express')
-const requestLogger = require('./middlewares/request_logger')
 
+const express = require('express')
 const app = express()
 const port = process.env.PORT || 4567
+
 const db = require('./db')
+
+const Station = require('./models/stations.js')
+const requestLogger = require('./middlewares/request_logger')
 
 app.use(express.json())
 
@@ -15,6 +18,11 @@ app.use(requestLogger)
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
+})
+
+app.get('/api/stations', (req, res) => {
+    Station.findAll()
+        .then(data => res.json(data))
 })
 
 app.listen(port, () => {
