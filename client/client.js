1d15315aeeac382f77fd4e5940cd7708f24cc1b3
ogandi <ogandisouw@gmail.com>
2024-10-22 10:57:35 +1100

    lat long added to div

--- client/client.js
+++ client/client.js
@@ -1,3 +1,5 @@
+const centerCoords_div = document.getElementById('center-coords')
+
 let map;
 
 async function initMap() {
@@ -7,6 +9,16 @@ async function initMap() {
     map = new Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
+    });
+
+    let center = map.getCenter()
+    centerCoords_div.textContent = center
+
+    google.maps.event.addListener(map, 'dragend', () => {
+        const newCenter = map.getCenter();
+        centerCoords_div.textContent = newCenter
+
+        console.log('New center:', newCenter.toJSON());
     });
 }
 
